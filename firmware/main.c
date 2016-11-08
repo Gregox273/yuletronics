cfcc1ed7853c200f6a9e86fbc7f6cec9110ef841
Gregory Brooks <gregox273@gmail.com>
2016-11-08 01:14:54 +0000

    Work on LED server/client threads

--- firmware/main.c
+++ firmware/main.c
@@ -4,38 +4,24 @@
 
 #include "ch.h"
 #include "hal.h"
-// #include "ch_test.h"
+#include <stdlib.h>
 
-static void pwmpcb(PWMDriver *pwmp);
+// Colour macros (for 3 bit message format)
+#define COL_RED 1
+#define COL_GRN 2
+#define COL_BLU 4
 
-/*
- * PWM configuration structure.
- */
-static PWMConfig pwm2cfg = {
-  10000,                                 /* 10kHz PWM clock frequency.   */
-  10,                                    /* PWM period 0.01S (in ticks).    */
-  pwmpcb,
-  {
-    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-    {PWM_OUTPUT_ACTIVE_HIGH, NULL}
-  },
-  0
-};
+#define COL_YLW (COL_RED + COL_GRN)
+#define COL_CYN (COL_GRN + COL_BLU)
+#define COL_MAG (COL_RED + COL_BLU)
 
-static PWMConfig pwm3cfg = {
-  10000,                                 /* 10kHz PWM clock frequency.   */
-  10,                                    /* PWM period 0.01S (in ticks).    */
-  pwmpcb,
-  {
-    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
-    {PWM_OUTPUT_DISABLED, NULL},
-    {PWM_OUTPUT_DISABLED, NULL}
-  },
-  0
-};
+#define COL_WHT (COL_RED + COL_GRN + COL_BLU)
+
+#define PWM_PERIOD 1024  // ~0.01s in systicks
+
+
+// Shared variable to define current LED channel state (3 bits)
+static uint8_t current_state = 1;  // Current led channel state (channels 1 to 3)
 
 /*
  * PWM cyclic callback.
@@ -45,35 +31,88 @@ static void pwmpcb(PWMDriver *pwmp) {
  
   (void)pwmp;
   // Do things
+  
   /* Increment fade completion/progress variable
    * which varies from 0 to brighness
    */
 }
 
+
+/*
+ * PWM configuration structure.
+ */
+static PWMConfig pwm2cfg = {
+  10000,                                 /* 10kHz PWM clock frequency.   */
+  PWM_PERIOD,                            /* PWM period 0.01S (in ticks).    */
+  pwmpcb,
+  {
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL}
+  },
+  0,
+  0,
+};
+
+static PWMConfig pwm3cfg = {
+  10000,                                 /* 10kHz PWM clock frequency.   */
+  PWM_PERIOD,                            /* PWM period 0.01S (in ticks).    */
+  pwmpcb,
+  {
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_ACTIVE_HIGH, NULL},
+    {PWM_OUTPUT_DISABLED, NULL},
+    {PWM_OUTPUT_DISABLED, NULL}
+  },
+  0,
+  0,
+};
+
+
 /*
  * LED fader thread, times are in milliseconds.
  */
-static WORKING_AREA(waLEDS, 128);
-static msg LEDS(void * arg) {
-  Mailbox* mbox = (Mailbox *)arg;
+static THD_WORKING_AREA(waLEDS, 128);
+static THD_FUNCTION(LEDS, arg) {
+  mailbox_t* mbox = (mailbox_t *)arg;
   msg_t msg, result;
+  bool current_group = false; /* led group that is currently active 
+                               * false for led group 1, true for group 2 */
   
   while (true) {
-     result = chMBFetch(mbox, &msg, TIME_INFINITE);  // Fetch LED request
-        if(result == RDY_OK) {
-			//if(msg & 1) - demo of if statement checking a bit of msg?
-			
-            // Do PWM things
+      result = chMBFetch(mbox, &msg, TIME_INFINITE);  // Fetch LED request
+      if(result == MSG_OK) {
+        uint8_t brightness = (msg & 56) / 8;       // Separate brightness value into a 0-8 (decimal) value
+        brightness = PWM_PERIOD * brightness / 8;  // Now express as a number of systicks (max 1 wave period) 
+        // Do PWM things
+        current_group = !current_group; // Other group is now active
+        for(uint16_t dcycle = 0; dcycle < brightness; dcycle++){
+
+          !!!!!
+          //fade down existing colours
+          !!!!!
+
+          if (current_group){
+            //if (msg & COL_RED);
+	    //if (msg & COL_GRN);
+	    //if (msg & COL_BLU);
+          }
+
+          else {
+            //if (msg & COL_RED);
+	    //if (msg & COL_GRN);
+	    //if (msg & COL_BLU);
+	  }
+          chThdSleepMicroseconds(500);  // Adjust to control time taken to fade
         }
+      }
   }
-  
-  return 0;
 }
 
-static WORKING_AREA(waLEDC, 128);
-static msg_t LEDC(void * arg) {
-	Mailbox* mbox = (Mailbox *)arg;
-	uint8_t current_state = 1;  // Current led channel state (channels 1 to 3)
+static THD_WORKING_AREA(waLEDC, 128);
+static THD_FUNCTION(LEDC, arg) {
+	mailbox_t* mbox = (mailbox_t *)arg;
 	
 	while(true){
 	  uint8_t next_state = 0;
@@ -87,14 +126,14 @@ static msg_t LEDC(void * arg) {
 	  }
 	  next_state = next_state & 7;  //Ensure only 3 bits can be set 
 	  //chMBPost(mbox, next_state & brightness bits, TIME_INFINITE);
+          chMBPost(mbox, next_state || 56, TIME_INFINITE);  // Max brightness
 	  /* Message format (LSB is #0):
-	   * 0 to 2 : led channels to light next
+	   * 0 to 2 : led channels to light next (LSB -> MSB : R,G,B)
 	              note that current number of lit channels + next no. of lit channels <= 4
-       * 3 to 5 : brightness level (0 to 7 in decimal)
+           * 3 to 5 : brightness level (0 to 7 in decimal)
 	   */
 	  chThdSleepMilliseconds(5000);  // Change colours every 5 seconds
     }
-	return 0;
 }
 
 /*
@@ -113,9 +152,9 @@ int main(void) {
   chSysInit();
 
   //Create thread mailbox
-  Mailbox mbox;
+  mailbox_t mbox;
   msg_t mbox_buffer[3];
-  chMBInit(&mbox,mbox_buffer, 3);
+  chMBObjectInit(&mbox,mbox_buffer, 3);
   
   // Initialise PWM drivers
   pwmStart(&PWMD2, &pwm2cfg);
